5a1447a6d93f36734971a80d099be5bf80fb5e32
zbob889 <zroberts889@gmail.com>
2022-06-09 15:34:47 +0200

    got basic math functions up

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function add(a, b){
+    let sum = a + b;
+    return sum;
+}
+
+function subtract(a, b){
+    let sum = a - b;
+    return sum;
+}
+
+function multiply(a, b){
+    let sum = a * b;
+    return sum;
+}
+
+function divide(a, b){
+    let sum = a/b;
+    return sum;
+}
