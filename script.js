f8c2579bff12f534189f99514f7ec3fca5348c7b
zbob889 <zroberts889@gmail.com>
2022-06-09 16:39:45 +0200

    got operate function working

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-
-
 function add(a, b){
     let sum = a + b;
     return sum;
-}
+};
 
 function subtract(a, b){
     let sum = a - b;
     return sum;
-}
+};
 
 function multiply(a, b){
     let sum = a * b;
     return sum;
-}
+};
 
 function divide(a, b){
     let sum = a/b;
     return sum;
 }
+
+function operate(operator, firstNumber, secondNumber){
+    let sum;
+
+    if(operator === 'add'){
+        sum = add(firstNumber, secondNumber);
+    } else if(operator === 'substract'){
+        sum = subtract(firstNumber, secondNumber);
+    } else if(operator === 'multiply'){
+        sum = multiply(firstNumber, secondNumber);
+    } else if(operator === 'divide'){
+        sum = divide(firstNumber, secondNumber);
+    } else {
+        return 'ERROR';
+    };
+
+    return sum;
+};
