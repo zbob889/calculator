a1aa32bcaa9cda31eabf73c347b30514a92aa42c
zbob889 <zroberts889@gmail.com>
2022-06-10 11:49:16 +0200

    made constants for the buttons

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const zeroButton = document.getElementById('zero');
+const oneButton = document.getElementById('one');
+const twoButton = document.getElementById('two');
+const threeButton = document.getElementById('three');
+const fourButton = document.getElementById('four');
+const fiveButton = document.getElementById('five');
+const sixButton = document.getElementById('six');
+const sevenButton = document.getElementById('seven');
+const eightButton = document.getElementById('eight');
+const nineButton = document.getElementById('nine');
+const equalsButton = document.getElementById('equals');
+const addButton = document.getElementById('add');
+const subtractButton = document.getElementById('subtract');
+const multiplyButton = document.getElementById('multiply');
+const divideButton = document.getElementById('divide');
+const clearButton = document.getElementById('ac');
+
+
+
 function add(a, b){
     let sum = a + b;
     return sum;
